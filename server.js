0d8ee8072953f59a9606dd77d214fcaa6140815d
Sreenum053 <srinivasulum053@gmail.com>
2024-08-29 22:16:56 +0530

    added post method

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 
-
+app.post("/user", (req, res)=>{
+    console.log("Hello World!!");
+})
 
 app.listen(3000, ()=> console.log("Project running port 3000"));
 
